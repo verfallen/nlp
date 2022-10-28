d4e27e8697731cc20a965f0ce304f9f6ef835e45
cocoder <cocoder@qq.com>
2022-10-28 16:38:22 +0800

    保存词频

--- word2vec/corpus.py
+++ word2vec/corpus.py
@@ -9,15 +9,15 @@ import numpy as np
 
 
 def rm_sign(string):
-    string = re.sub("[\.\!_,\$\(\)\"\'\]\[！!\?，。？、~@#￥……&]+", "", string)
+    string = re.sub("[\.\!_,\$\(\)\"'\]\[！!\?，。？、~@#￥……&]+", "", string)
     return string
 
 
-def load_data(file_path='./data/corpus.txt'):
-    with open(file_path, 'r') as f:
+def load_data(file_path="../data/corpus.txt"):
+    with open(file_path, "r") as f:
         for line in f:
             line = line.strip()
-            if (len(line) == 0):
+            if len(line) == 0:
                 continue
             yield rm_sign(line.lower()).split()
 
@@ -32,17 +32,19 @@ class Corpus(object):
         self.idx2word = []
         self.word2idx = {}
         self.word_count = 0
-        # self.windows = []
-        # self.dataset = []
+        self.word_freq = []
 
         flatten = lambda lines: [word for line in lines for word in line]
 
-        counter = Counter(flatten(data)).most_common()
+        all_words = flatten(data)
+
+        counter = Counter(all_words).most_common()
         for word, count in counter:
             self.idx2word.append(word)
             self.word2idx[word] = self.word_count
             self.word_count += 1
             self.vocab.append(word)
+            self.word_freq.append(count / len(all_words))
 
     def encode(self, word):
         if type(word) == list:
