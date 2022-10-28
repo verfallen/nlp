3e8800c57d7787052fac48f1c082d1ad03ba7e55
verfallen <cocoder@qq.com>
2022-10-28 21:24:01 +0800

    负采样的sg模型

---
+++ word2vec/skip_gram_neg_sample.py
@@ -0,0 +1,26 @@
+from turtle import forward
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class SkipGram(nn.Module):
+    def __init__(self, vocab_size, embedding_dim) -> None:
+        super().__init__()
+        self.embedding_in = nn.Embedding(vocab_size, embedding_dim)
+        self.embedding_out = nn.Embedding(vocab_size, embedding_dim)
+
+    def forward(self, center, context, neg):
+        v = self.embedding_in(center)
+        u = self.embedding_out(context)
+        u_neg = -self.embedding_out(neg)
+
+        pos_score = torch.bmm(u,v.transpose(1,2)).squeeze(-1)
+        neg_score = torch.bmm(u_neg, v.transpose(1,2)).squeeze(-1)
+
+        return self.loss(pos_score, neg_score)
+
+    def loss(self, target_scores, neg_scores):
+        prob = F.logsigmoid(target_scores).sum(1) + F.logsigmoid(neg_scores).sum(1)
+        return -torch.mean(prob)
+        
