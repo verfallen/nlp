6b40b28b930cc5d8712b6a99c882930a2c080649
wangxiaoli <cocoder12138@gmail.com>
2022-11-29 10:06:03 +0800

    加入CRF层

--- bilstm_crf_ner/model.py
+++ bilstm_crf_ner/model.py
@@ -3,9 +3,10 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from config import *
+from torchcrf import CRF
 
 
-class BILSTM(nn.Module):
+class Bilstm_crf(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.embedding = nn.Embedding(VOCAB_SIZE, EMBEDDING_DIM)
@@ -13,19 +14,24 @@ class BILSTM(nn.Module):
             EMBEDDING_DIM, HIDDEN_SIZE, batch_first=True, bidirectional=True
         )
         self.linear = nn.Linear(2 * HIDDEN_SIZE, TARGET_SIZE)
+        self.crf = CRF(TARGET_SIZE, batch_first=True)
 
     def _get_lstm_feature(self, input):
         embed = self.embedding(input)
         out, _ = self.lstm(embed)
         return self.linear(out)
 
-    def forward(self, input):
+    def forward(self, input, mask):
         out = self._get_lstm_feature(input)
-        return out
+        return self.crf.decode(out, mask)
+
+    def loss_fn(self, input, target, mask):
+        y_pred = self._get_lstm_feature(input)
+        return -self.crf.forward(y_pred, target, mask, reduction="mean")
 
 
-if __name__ == "__main__":
-    model = BILSTM()
-    input = torch.randint(1, 3000, (100, 50))
-    print(model)
-    print(model(input).shape)
+# if __name__ == "__main__":
+#     model = Bilstm_crf()
+#     input = torch.randint(1, 3000, (100, 50))
+#     print(model)
+#     print(model(input).shape)
